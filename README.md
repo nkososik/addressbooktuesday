206ff011a68e27bead552d81605b0b964b9216a6
Vanessa Guerrero <vamarilug@gmail.com>
2020-06-16 10:26:46 -0700

    Includes specs in README.md, includes all necessary business logic and adds CSS and index.html files
    Co-Authored-By: Nikolas Kososik <nikolas.kososik@gmail.com>

--- README.md
+++ README.md
@@ -1,26 +1,53 @@
-# Webpage: Title
+# Webpage: Address Book
 
-#### What is this for, 6.x.2020
+#### Address Book for Epicodus, 6.16.2020
 
-#### By Vanessa Guerrero
+#### By Nikolas Kososik, Vanessa Guerrero
 
 ## Description
 
-The purpose of this project is to create a survey using business and user interface logic.
+The purpose of this application is to collect contact information from a user and populate an address book.
 
 ## Setup/Installation Requirements
 
-* Find the repository of this application [here]() and click the green "Clone or Download Folder" to download a zip file with all its contents.
+* Find the repository of this application [here](https://github.com/vguer/addressbooktuesday.git) and click the green "Clone or Download Folder" to download a zip file with all its contents.
 * Once file is downloaded, open with Visual Studio Code or the text editor of your choice to view the code.
 * To view and intereact with the webpage using Visual Studio Code, click on Terminal > New Terminal. Once a terminal opens, type "open index.html" and hit enter. This will open the page onto a browser window.
 
 
 ## Specifications
 
-**Behavior**: Program will return x if x
-  * Input: example
-  * Output: true/false
+**Behavior** Takes user input and returns it
+* Input: text
+* Output text
 
+**Behavior** Checks to see if input is empty 
+* Input: empty input
+* Output "Empty or incorrect input"
+
+**Behavior** It creates an Address Book
+* Input: no input necessary 
+* Output: Address Book Object: {contacts: [], currentId: 0}
+
+**Behavior** Program creates a contact
+* Input: User's "first name, last name, phone number, email address, physical address"
+* Output: Contact Object: {firstName, lastName, phoneNumber, emailAddress, physicalAddress}
+
+**Behavior** Program provides contact object's properties
+* Input: "Click" on contact name
+* Output: firstName, lastName, phoneNumber, emailAddress, physicalAddress
+
+**Behavior** Add contact to the address book and assigns Id
+* Input: Contact Object: {firstName: "Sam" , lastName: "Lot" , phoneNumber: "5555556565", emailAddress: "myemail@gmail.com", physicalAddress: "420 Paper Street"}
+* Output: AddressBook Object has 1 contact: {id: 1, firstName: "Sam" , lastName: "Lot" , phoneNumber: "5555556565", emailAddress: "myemail@gmail.com", physicalAddress: "420 Paper Street"}
+
+**Behavior** Program finds contact with given id
+* Input: id 7
+* Output: Contact Object: {id: 7, firstName: "Rex" , lastName: "Burg" , phoneNumber: "1112223434", emailAddress: "youremail@yahoo.com", physicalAddress: "55 E Trail Mix Lane"}
+
+**Behavior** Program deletes contact with given id
+* Input: id 7
+* Output: Contact Object with id 7 has been deleted from the address book
 ## Known Bugs
 
 There are no known bugs.
@@ -36,9 +63,10 @@ This project is created using:
 * CSS
 * JavaScript
 * JQuery
+* Bootstrap
 
 ### License
 
 This project is licensed under the [MIT License](https://opensource.org/licenses/MIT).
 
-Copyright (c) 2020 Vanessa Guerrero 
+Copyright (c) 2020 Nikolas Kososik, Vanessa Guerrero 
--- js/scripts.js
+++ js/scripts.js
@@ -1,16 +1,11 @@
-$(document).ready(function() {
 
-  event.preventDefault();
-
-
-
-  //Business Logic for AddresBook ------
+  // Business Logic for AddressBook
 function AddressBook() {
-  this.contacts = []; 
-  this.currentId= 0;
+  this.contacts = [];
+  this.currentId = 0;
 }
 
-AddressBook.prototype.addContact = function (contact) {
+AddressBook.prototype.addContact = function(contact) {
   contact.id = this.assignId();
   this.contacts.push(contact);
 }
@@ -22,35 +17,99 @@ AddressBook.prototype.assignId = function() {
 
 AddressBook.prototype.findContact = function(id) {
   for (let i=0; i< this.contacts.length; i++) {
-    if (this.contacts[i]) {    
+    if (this.contacts[i]) {     
       if (this.contacts[i].id == id) {
         return this.contacts[i];
       }
-    }                         
+    }                          
   };
   return false;
 }
 
+  if(this.findContact(id)) {
+    this.findContact(id).firstName = firstName;
+    this.findContact(id).lastName = lastName;
+    this.findContact(id).phoneNumber = phoneNumber;
+  } else {
+    return false;
+  }
+
+
 AddressBook.prototype.deleteContact = function(id) {
   for (let i=0; i< this.contacts.length; i++) {
-    if (this.contacts[i]) {    
+    if (this.contacts[i]) {     
       if (this.contacts[i].id == id) {
         delete this.contacts[i];
         return true;
       }
-    }                         
+    }                          
   };
   return false;
 }
 
-// Business Logic for Contacts --------
-function Contact (firstName, lastName, phoneNumber) {
+
+// Business Logic for Contacts
+function Contact(firstName, lastName, phoneNumber) {
   this.firstName = firstName;
   this.lastName = lastName;
   this.phoneNumber = phoneNumber;
 }
 
 Contact.prototype.fullName = function() {
-  return this.firstName + "" + this.lastName;
+  return this.firstName + " " + this.lastName;
 }
+
+// User Interface Logic
+let addressBook = new AddressBook();
+
+function displayContactDetails(addressBookToDisplay) {
+  let contactsList = $("ul#contacts");
+  let htmlForContactInfo = "";
+  addressBookToDisplay.contacts.forEach(function(contact) {
+    htmlForContactInfo += "<li id=" + contact.id + ">" + contact.firstName + " " + contact.lastName + "</li>";
+  });
+  contactsList.html(htmlForContactInfo);
+}
+
+function showContact(contactId) {
+  const contact = addressBook.findContact(contactId);
+  $("#show-contact").show();
+  $(".first-name").html(contact.firstName);
+  $(".last-name").html(contact.lastName);
+  $(".phone-number").html(contact.phoneNumber);
+  let buttons = $("#buttons");
+  buttons.empty();
+  buttons.append("<button class='deleteButton' id=" +  + contact.id + ">Delete</button>");
+}
+
+function attachContactListeners() {
+  $("ul#contacts").on("click", "li", function() {
+    showContact(this.id);;
+  });
+  $("#buttons").on("click", ".deleteButton", function() {
+    addressBook.deleteContact(this.id);
+    $("#show-contact").hide();
+    displayContactDetails(addressBook);
+  });
+};
+
+$(document).ready(function(){
+  attachContactListeners();
+  $("form#new-contact").submit(function(event){
+    event.preventDefault();
+    let firstName = $("#new-first-name").val();
+    let lastName = $("#new-last-name").val();
+    let phoneNumber = $("#new-phone-number").val();
+    if(!lastName || !firstName || !phoneNumber){
+      alert("Empty or incorrect input");
+      return;
+    }
+    $("input#new-first-name").val("");
+    $("input#new-last-name").val("");
+    $("input#new-phone-number").val("");
+
+    let contact = new Contact(firstName, lastName, phoneNumber);
+    addressBook.addContact(contact);
+    displayContactDetails(addressBook);
+  });
 });
